336f1747f61cb152e7cfedb6d21380e079c3cd1c
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-09-29 18:43:41 +0200

    log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
 
   get 'users/new'
 
@@ -10,6 +9,11 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
